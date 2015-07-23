0211352e38c8a442f36e1275aceaa2a5fe3fa23a
Wouter Danes <wouter.danes@gmail.com>
2015-07-23 20:44:59 +0200

    Added image variants.

--- site/src/main/java/org/example/beans/Banner.java
+++ site/src/main/java/org/example/beans/Banner.java
@@ -1,25 +1,11 @@
 package org.example.beans;
-/*
- * Copyright 2014 Hippo B.V. (http://www.onehippo.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoHtml;
 import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.example.beans.Imageset;
 
 @HippoEssentialsGenerated(internalName = "gogreen:bannerdocument")
 @Node(jcrType = "gogreen:bannerdocument")
@@ -34,13 +20,13 @@ public class Banner extends BaseDocument {
 		return getHippoHtml("gogreen:content");
 	}
 
-	@HippoEssentialsGenerated(internalName = "gogreen:image")
-	public HippoGalleryImageSet getImage() {
-		return getLinkedBean("gogreen:image", HippoGalleryImageSet.class);
-	}
-
 	@HippoEssentialsGenerated(internalName = "gogreen:link")
 	public HippoBean getLink() {
 		return getLinkedBean("gogreen:link", HippoBean.class);
+	}
+
+	@HippoEssentialsGenerated(internalName = "gogreen:image")
+	public Imageset getImage() {
+		return getLinkedBean("gogreen:image", Imageset.class);
 	}
 }
