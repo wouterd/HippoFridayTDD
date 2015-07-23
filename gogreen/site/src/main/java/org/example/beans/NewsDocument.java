421d13eb4c4985b432c7f70e26c1d8364cb53fc5
Wouter Danes <wouter.danes@gmail.com>
2015-07-23 23:31:50 +0200

    Moved hippo project to it's own folder

---
+++ gogreen/site/src/main/java/org/example/beans/NewsDocument.java
@@ -0,0 +1,109 @@
+package org.example.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+import org.onehippo.cms7.essentials.components.rest.adapters.HippoHtmlAdapter;
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Calendar;
+
+@XmlRootElement(name = "newsdocument")
+@XmlAccessorType(XmlAccessType.NONE)
+@HippoEssentialsGenerated(internalName = "gogreen:newsdocument")
+@Node(jcrType = "gogreen:newsdocument")
+public class NewsDocument extends HippoDocument {
+	/** 
+	 * The document type of the news document.
+	 */
+	public final static String DOCUMENT_TYPE = "gogreen:newsdocument";
+	private final static String TITLE = "gogreen:title";
+	private final static String DATE = "gogreen:date";
+	private final static String INTRODUCTION = "gogreen:introduction";
+	private final static String IMAGE = "gogreen:image";
+	private final static String CONTENT = "gogreen:content";
+	private final static String LOCATION = "gogreen:location";
+	private final static String AUTHOR = "gogreen:author";
+	private final static String SOURCE = "gogreen:source";
+
+	/** 
+	 * Get the title of the document.
+	 * @return the title
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:title")
+	public String getTitle() {
+		return getProperty(TITLE);
+	}
+
+	/** 
+	 * Get the date of the document.
+	 * @return the date
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:date")
+	public Calendar getDate() {
+		return getProperty(DATE);
+	}
+
+	/** 
+	 * Get the introduction of the document.
+	 * @return the introduction
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:introduction")
+	public String getIntroduction() {
+		return getProperty(INTRODUCTION);
+	}
+
+	/** 
+	 * Get the main content of the document.
+	 * @return the content
+	 */
+	@XmlJavaTypeAdapter(HippoHtmlAdapter.class)
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:content")
+	public HippoHtml getContent() {
+		return getHippoHtml(CONTENT);
+	}
+
+	/** 
+	 * Get the location of the document.
+	 * @return the location
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:location")
+	public String getLocation() {
+		return getProperty(LOCATION);
+	}
+
+	/** 
+	 * Get the author of the document.
+	 * @return the author
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:author")
+	public String getAuthor() {
+		return getProperty(AUTHOR);
+	}
+
+	/** 
+	 * Get the source of the document.
+	 * @return the source
+	 */
+	@XmlElement
+	@HippoEssentialsGenerated(internalName = "gogreen:source")
+	public String getSource() {
+		return getProperty(SOURCE);
+	}
+
+	@HippoEssentialsGenerated(internalName = "gogreen:image")
+	public Imageset getImage() {
+		return getLinkedBean("gogreen:image", Imageset.class);
+	}
+}
